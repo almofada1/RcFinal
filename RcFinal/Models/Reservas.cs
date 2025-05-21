dc74f47b1554f7758668ff7d96d5cf5c0cd79582
almofada1 <fvmarto@gmail.com>
2025-05-21 16:33:23 +0100

    package error

--- RcFinal/Models/Reservas.cs
+++ RcFinal/Models/Reservas.cs
@@ -24,8 +24,6 @@ public class Reservas
     public int RoomId { get; set; }
     [ForeignKey(nameof(RoomId))]
     public Quartos Quartos { get; set; } = default!;
-
-    [Required]
     public int PackageId { get; set; }
     public Package? Package { get; set; }
 
